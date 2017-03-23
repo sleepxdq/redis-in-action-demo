2278e7ec1cccae62dedf9e5a383805d78206bb89
sleepxdq <xu_dongqiang@foxmail.com>
2017-03-24 00:15:26 +0800

    BUCT

---
+++ BUCT/src/main/java/com/xdq/buct/BuctApplication.java
@@ -0,0 +1,12 @@
+package com.xdq.buct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuctApplication.class, args);
+	}
+}
---
+++ BUCT/src/test/java/com/xdq/buct/BuctApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xdq.buct;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BuctApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
